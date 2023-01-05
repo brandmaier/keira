23f434fa48c7931f9a7110de7e52f3c5b8c78070
Andreas Brandmaier <andy@brandmaier.de>
2023-01-05 15:10:18 +0100

    make points an argument

--- R/generate.R
+++ R/generate.R
@@ -8,13 +8,16 @@
 #'
 
 generate <- function(files, n = 1, title = "", name="keira-klausur", course="M99",
-                     showpoints = TRUE) {
+                     showpoints = TRUE, points = NULL) {
 
-set.seed(203)
+if (is.null(points)) {
+  points <- rep(1, length(files))
+}
+
 ex1 <- exams2nops(files, n = n,
                   dir = "demo_nops_pdf", name = name,
                   #date = "2022-02-29",
-                  points = rep(1, length(files)), # Immer 1 Punkt pro MC-Frage
+                  points = points, # Immer 1 Punkt pro MC-Frage
                   showpoints = showpoints,
                   samepage = TRUE, # questions should stay on the same page
                   duplex = FALSE,language = "de",
@@ -28,7 +31,7 @@ ex1 <- exams2nops(files, n = n,
                   intro = paste0(c(
                     "Hinweise: \\\\",
                     "\\begin{itemize}",
-                    "\\item Die Klausur besteht aus 3 geschlossenen Fragen mit vorgegebenen Antwortm\\\"oglichkeiten (jeweils eine Antwort ist richtig) ",
+                    "\\item Die Klausur besteht aus ",length(files)," geschlossenen Fragen mit vorgegebenen Antwortm\\\"oglichkeiten (jeweils eine Antwort ist richtig) ",
                     "\\item Bitte pr\\\"ufen Sie vor der Abgabe, dass Sie alle Fragen auf dem Deckblatt beantwortet haben. Nur die Antworten auf dem Deckblatt werden ber\\\"ucksichtigt.",
                     "\\item Viel Erfolg!",
                     "\\end{itemize}",
--- test/test_converter.R
+++ test/test_converter.R
@@ -12,3 +12,11 @@ converter("misc/testcase-multiple-answers-correct.docx")
 
 converter("../../../Lehre/M25-P-WS-2022/9.Klausur/Klausurfragen-Pool2.docx", debug=FALSE,
           include_tags = "#probeklausur", exclude_tags=c())
+
+
+scanExam("temp_nops_scan/demo_nops_pdf.zip")
+
+evaluate("temp_nops_scan/output.zip", solutions = "demo_nops_pdf/keira-klausur.rds",
+         rule="false")
+         #labels=c("1,0","1,3","1,7","2,0","2,3","2,7","3,0","3,3","3,7","4,0","n.b."),
+         #marks=seq(0.5, 1, length.out=11 )[1:10])
