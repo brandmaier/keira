270b258a55390433235f264cf35465e1e8000e19
Andreas Brandmaier <andy@brandmaier.de>
2023-04-25 12:31:31 +0200

    renamed label function

--- R/marks_and_labels.R
+++ R/marks_and_labels.R
@@ -1,4 +1,4 @@
-getMarksAndLabels <- function(maxpoints) {
+get_marks_and_labels <- function(maxpoints) {
 #  maxpoints <-
   labels <- c("n.b.","4,0","3,7","3,3","3,0","2,7","2,3","2,0","1,7","1,3","1,0")
   marks <- c(0.5, 0.545, 0.606, 0.652, 0.697, 0.758, 0.803, 0.848, 0.909, 0.955)*maxpoints
