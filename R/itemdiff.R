ebd2569413f1c3c247079a8cf757a13405ab04ed
Andreas Brandmaier <andy@brandmaier.de>
2023-09-20 09:41:02 +0200

    fixed imports

--- R/itemdiff.R
+++ R/itemdiff.R
@@ -12,21 +12,25 @@ plot_item_difficulty <- function(eval_file = "nops_eval.csv",
   n_items <- length(pcols_id)
   subdat <- pts[, c(1:8, pcols_id)]
 
-  color_table <- tibble(
-    lab = c("Difficult", "Normal", "Easy"),
-    col = cols
-  )
+#  color_table <- tibble(
+#    lab = c("Difficult", "Normal", "Easy"),
+#    col = cols
+#  )
 
   names(subdat)[1:n_items + 8] <-
     paste0("Item ", stringr::str_pad(1:n_items, pad = "0", width = 3))
   npts <-
-    subdat[, -(1:8)] %>% pivot_longer(1:n_items) %>% group_by(name) %>% summarise(avg =
-                                                                                    mean(value)) %>% mutate(avgn = avg / maxpoints)
+    subdat[, -(1:8)] %>% tidyr::pivot_longer(1:n_items) %>% dplyr::group_by(name) %>% dplyr::summarise(avg =
+                                                                                    mean(value)) %>% dplyr::mutate(avgn = avg / maxpoints)
   npts$cl <- ifelse(npts$avgn <= 0.10, "Difficult", "Normal")
   npts$cl <- ifelse(npts$avgn >= 0.90, "Easy", npts$cl)
   npts$cl <- factor(npts$cl, levels = c("Easy", "Normal", "Difficult"))
-  npts %>% ggplot(aes(avgn, name, fill = cl)) + geom_col() + xlab("Schwierigkeit") +
-    ylab("Item") +
-    scale_fill_manual(values = color_table$col, drop = FALSE) + xlim(0, 1)
+  npts %>% ggplot2::ggplot(
+    ggplot2::aes(avgn, name, fill = cl)) +
+    ggplot2::geom_col() +
+    ggplot2::xlab("Schwierigkeit") +
+    ggplot2::ylab("Item") +
+    ggplot2::scale_fill_manual(values = cols, drop = FALSE) +
+    ggplot2::xlim(0, 1)
 
 }
