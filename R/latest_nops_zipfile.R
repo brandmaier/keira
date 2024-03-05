41d26b956421553a7ca59367d204b3275d1e58d0
Andreas Brandmaier <andy@brandmaier.de>
2024-03-05 11:38:39 +0100

    added finder for latest zip file

---
+++ R/latest_nops_zipfile.R
@@ -0,0 +1,11 @@
+latest_nops_zipfile <- function(path="png") {
+  files = list.files(path="png/", pattern=".*nops_scan_.*zip",full.names = TRUE)
+  dates = sapply(files, function(x) {
+    date_string <- (regmatches(x, regexpr("\\d+", x)))
+    posix_date <- as.POSIXct(date_string, format = "%Y%m%d%H%M%S")
+  },simplify = FALSE)
+
+  mtime = sapply(files, function(file){file.info(file)$mtime})
+
+  files[which.max(mtime)]
+}
