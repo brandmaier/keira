6c82a5d04cecbd887d5f928782fbe13eb43a36d1
Andreas Brandmaier <andy@brandmaier.de>
2024-03-22 11:09:53 +0100

    fixed num_items

--- R/item_summary.R
+++ R/item_summary.R
@@ -2,7 +2,9 @@ item_summary <- function() {
 
   itd <- item_discrimination()
 
-  df <- data.frame(paste0(item,1:num_items), itd)
+  num_items <- length(itd)
+
+  df <- data.frame(paste0("item",1:num_items), itd)
 
   names(df) <- c("Item", "Discrimination")
 
