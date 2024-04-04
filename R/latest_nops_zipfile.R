90d0848074d32136a8b2446ba3742103da995400
Andreas Brandmaier <andy@brandmaier.de>
2024-04-04 07:06:42 +0200

    fixed path

--- R/latest_nops_zipfile.R
+++ R/latest_nops_zipfile.R
@@ -1,5 +1,5 @@
 latest_nops_zipfile <- function(path="png") {
-  files = list.files(path="png/", pattern=".*nops_scan_.*zip",full.names = TRUE)
+  files = list.files(path=paste0(path,"/"), pattern=".*nops_scan_.*zip",full.names = TRUE)
   dates = sapply(files, function(x) {
     date_string <- (regmatches(x, regexpr("\\d+", x)))
     posix_date <- as.POSIXct(date_string, format = "%Y%m%d%H%M%S")
